7277820b6fef68b9241a53b48cee7ec788f47322
JoshElias <josh.elias.dev@gmail.com>
2016-03-29 01:34:21 -0700

    added UpdateAll

--- save-foreign-key.js
+++ save-foreign-key.js
@@ -1,6 +1,7 @@
 var xloop = require("xloop");
 var resultCrawler = xloop.resultCrawler;
 var packageJSON = require("./package");
+var slugFuncs = require("./slug-funcs");
 
 
 
@@ -21,24 +22,61 @@ function saveForeignKey(Model, mixinOptions, ctx, foreignKeyName, finalCb) {
 
 
 function primitiveHandler(state, mixinOptions, finalCb) {
-    var Slug = state.models.slug;
 
-    Slug.findOne({
-        where: {
-            parentModelName: state.modelName,
-            baseKey: state.key
-        },
-        fields: {
-            id: true
+    // Get options from modelConfig
+    var slugOptions = {};
+    var modelSlugOptions = state.modelProperties[mixinOptions.mixinName];
+    if (typeof modelSlugOptions === "object") {
+        for (var key in modelSlugOptions) {
+            slugOptions[key] = modelSlugOptions[key];
         }
-    }, function(err, slug) {
-        if(err) return finalCb(err);
+    }
 
-        return slug.updateAttribute(state.foreignKeyName, state.ctx.instance.id, function(err) {
+    // Add any potential options from the client
+    if (state.ctx.req && state.ctx.req.body && typeof state.ctx.req.body.slugOptions === "object") {
+        for (var key in state.ctx.req.body.slugOptions) {
+            slugOptions[key] = state.ctx.req.body.slugOptions[key];
+        }
+    }
+
+    var linked = (  slugOptions.linked === undefined
+    || slugOptions.linked === null  )
+        ? true : slugOptions.linked;
+    var slug = (!linked && typeof slugOptions.slug === "string")
+        ? slugOptions.slug : slugify(state.data);
+
+    // Add slug modifiers
+    var prefixFunc = slugFuncs[slugOptions.prefixFunc];
+    if (typeof prefixFunc === "function") {
+        slug = prefixFunc(instance) + slug;
+    } else if (typeof slugOptions.prefix === "string") {
+        slug = slugOptions.prefix + slug;
+    }
+
+    // Update the slug with the new foreign key
+    var update = {
+        "$set": {}
+    }
+    update["$set"][state.foreignKeyName] = state.ctx.instance.id.toString();
+
+    var Slug = state.models.slug;
+    return Slug.updateAll(
+        {
+            parentModelName: state.modelName,
+            baseKey: state.key,
+            slug: slug
+        },
+        update,
+        { allowExtendedOperators: true },
+        function(err, info) {
             return finalCb(err);
         });
-    });
 }
+
+function slugify(string) {
+    return (string) ? string.toLowerCase().replace(/-+/g, '').replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '') : '';
+}
+
 
 
 module.exports = saveForeignKey;
